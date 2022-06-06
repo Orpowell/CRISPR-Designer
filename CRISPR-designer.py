6ed2517e7ebd75d0d97c50331aa37dd220ebbc5e
Oliver Powell <mail@oliverpowell.com>
2022-06-06 21:22:09 +0100

    - Changed output to optional input - by default will save to current working directory - otherwise will save to input directory IF valid

--- CRISPR-designer.py
+++ CRISPR-designer.py
@@ -389,6 +389,9 @@ def cmd_lineparser():
     if arguments.mutant not in amino_acids:
         parser.error('--mutant requires valid single amino acid code or "*" ')
 
+    if os.path.isdir(arguments.output) is False:
+        parser.error('--output requires valid directory')
+
     return arguments
 
 
